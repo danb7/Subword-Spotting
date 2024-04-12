f50de6516fcccee1db34737ea6cd031663c26447
Daniel Bazar <daniel.bazar@gmail.com>
2024-04-12 13:23:35 +0300

    refactoring classification prompts

--- subword_prompt_templates/ClassificationPrompts.py
+++ subword_prompt_templates/ClassificationPrompts.py
@@ -6,6 +6,8 @@ question = '''\
 Q: Does the word {word} contains a subword of a {category}?
 A: '''
 
+example = question + '{answer}'
+
 
 def generate_zero_shot(category, word):
     formated_question = question.format(category=category,
@@ -15,33 +17,52 @@ def generate_zero_shot(category, word):
 
 
 def generate_one_shot(category, word, positive_answer='Yes'):
-    return f'''\
+    example_word = classification_examples.positive_examples[category][0][0]
+    correspond_subword = classification_examples.positive_examples[category][0][1]
+    one_shot_question =  f'''\
 ### example question ###
-{classification_examples.example1[category].format(positive_answer=positive_answer)}
+{example.format(category=category, 
+                word=example_word, 
+                answer=positive_answer.format(example_word=example_word, 
+                                              example_subword=correspond_subword,
+                                              category=category))}
 ### actual question ###
 {generate_zero_shot(category, word)}'''
+    
+    return re.sub(r'\ba(?=\s+[aeiou])', 'an', one_shot_question)
 
 
-def generate_few_shot(category, word, positive_answer='Yes', negative_answer='No'):
-    return f'''\
-### example question 1 ###
-{classification_examples.example1[category].format(positive_answer=positive_answer)}
-### example question 2 ###
-{classification_examples.example2[category].format(negative_answer=negative_answer)}
-### example question 3 ###
-{classification_examples.example3[category].format(positive_answer=positive_answer)}
-### example question 4 ###
-{classification_examples.example4[category].format(negative_answer=negative_answer)}
-### actual question ###
-{generate_zero_shot(category, word)}'''
+def generate_few_shot(category, word, positive_answer='Yes', negative_answer='No', shots=4):
+    few_shot_question = ''
+    positive_examples = classification_examples.positive_examples[category]
+    negative_examples = classification_examples.negative_examples[category]
+    if shots/2 > len(positive_examples): raise Exception(f'too large shots. maximum is {shots}')
+    for i in range(int(shots/2)):
+        # positive example
+        few_shot_question += f'### example question {i*2+1} ###\n'
+        few_shot_question += example.format(category=category, 
+                                            word=positive_examples[i][0], 
+                                            answer=positive_answer.format(example_word=positive_examples[i][0],
+                                                                          example_subword=positive_examples[i][1],
+                                                                          category=category)) + '\n'
+        # negative example
+        few_shot_question += f'### example question {i*2+2} ###\n'
+        few_shot_question += example.format(category=category,
+                                            word=negative_examples[i], 
+                                            answer=negative_answer.format(example_word=negative_examples[i],
+                                                                          category=category)) + '\n'
+    
+    few_shot_question += '### actual question ###\n' 
+    few_shot_question += generate_zero_shot(category, word)
+    
+    return re.sub(r'\ba(?=\s+[aeiou])', 'an', few_shot_question)
 
 
 def generate_CoT(category, word, shot='one'):
-    # need to handle "the word" should be the example one
-    positive_answer = f'''Since the word contains a subword of \
-a {category}, the correct answer is Yes'''
-    negative_answer = f'''Since the word doesnt contains any subword \
-of a {category}, the correct answer is No'''
+    positive_answer = '''Since the word {example_word} contains the subword \
+{example_subword}, which is a {category}, the correct answer is Yes'''
+    negative_answer = '''Since the word {example_word} doesnt contains any subword \
+of a {category}, the correct answer is No''' 
     
     positive_answer = re.sub(r'\ba(?=\s+[aeiou])', 'an', positive_answer)
     negative_answer= re.sub(r'\ba(?=\s+[aeiou])', 'an', negative_answer)
--- subword_prompt_templates/few_shot_examples/classification_examples.py
+++ subword_prompt_templates/few_shot_examples/classification_examples.py
@@ -1,71 +1,16 @@
-example1 = {
-    'animal': '''\
-Q: Does the word million contains a subword of an animal?
-A: {positive_answer}.''',
+# positive_examples = {
+#     'animal': ['million', 'pants'], # beer, scatter
 
-    'color': '''\
-Q: Does the word credit contains a subword of a color?
-A: {positive_answer}.'''
-}
-
-example2 = {
-    'animal': '''\
-Q: Does the word book contains a subword of an animal?
-A: {negative_answer}.''',
-
-    'color': '''\
-Q: Does the word house contains a subword of a color?
-A: {negative_answer}.'''
-}
-
-example3 = {
-    'animal': '''\
-Q: Does the word pants contains a subword of an animal?
-A: {positive_answer}.''',
-
-    'color': '''\
-Q: Does the word abluent contains a subword of a color?
-A: {positive_answer}.'''
-}
-
-example4 = {
-    'animal': '''\
-Q: Does the word printer contains a subword of an animal?
-A: {negative_answer}.''',
-
-    'color': '''\
-Q: Does the word lamp contains a subword of a color?
-A: {negative_answer}.'''
-}
-
-
-### more positive examples if needed ###
-# ple2 = {
-#     'animal': '''\
-# Q: Does the word beer contains a subword of an animal?
-# A: Yes.''',
-
-#     'color': '''\
-# Q: Does the word stingray contains a subword of a color?
-# A: Yes.'''
+#     'color': ['credit', 'abluent'] # stingray, marigold
 # }
 
-# example3 = {
-#     'animal': '''\
-# Q: Does the word pants contains a subword of an animal?
-# A: Yes.''',
+positive_examples = {
+    'animal': [('million', 'lion'), ('pants', 'ant')], # beer, scatter
 
-#     'color': '''\
-# Q: Does the word abluent contains a subword of a color?
-# A: Yes.'''
-# }
+    'color': [('credit', 'red'), ('abluent', 'blue')] # stingray, marigold
+}
 
-# example4 = {
-#     'animal': '''\
-# Q: Does the word scatter contains a subword of an animal?
-# A: Yes.''',
-
-#     'color': '''\
-# Q: Does the word marigold contains a subword of a color?
-# A: Yes.'''
-# }
+negative_examples = {
+    'animal': ['book', 'printer'],
+    'color': ['house', 'lamp']
+}
