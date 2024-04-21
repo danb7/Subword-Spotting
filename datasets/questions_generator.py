782d22f7de45e6dd7d90832b2cfc4ed693a16264
lior <lior.krengel@gmail.com>
2024-04-21 20:01:23 +0300

    fixed evaluation

--- datasets/questions_generator.py
+++ datasets/questions_generator.py
@@ -13,6 +13,7 @@ sys.path.insert(0, parentdir)
 from subword_prompt_templates import MultiChoicePrompts, ClassificationPrompts
 from openai import OpenAI
 import gensim.downloader as dl
+word2vec_model = dl.load("word2vec-google-news-300")
 
 # Function to lemmatize words
 def lemmatize_word(word, stop_words):
@@ -45,39 +46,29 @@ def get_subwords_per_item(word2vec_model, types_dict, corpus_base_word, words_fr
     
     return subwords_list_tuple
 
-def generate_random_questions(selection_base_df, corpus_word_freq_df):
-    #selection_filtered_df = selection_base_df[(selection_base_df['Similarity'] <= 0.3) & (selection_base_df['Word_freq'] >= 10)]
-    filtered_df = selection_base_df[(selection_base_df['Similarity'] < 0.1) &
-                                    (selection_base_df['Word'].str.len() - 2 >= selection_base_df['Subword'].str.len())]
-    selection_filtered_df = filtered_df.loc[filtered_df.groupby('Subword')['Word_freq'].idxmax()]
-    grouped = selection_filtered_df.groupby('Category')
-    random_questions = []
-    for category, group in grouped:
-        # Randomly select 10 "Subword" entries weighted by "Subword_freq"
-        categories_subwords_distinct = group.drop_duplicates(subset=['Subword'])
-        choices_size = 5 if len(categories_subwords_distinct) >= 5 else len(categories_subwords_distinct)
-        selected_subwords = np.random.choice(categories_subwords_distinct['Subword'],
-                                            size=choices_size,
-                                            replace=False,
-                                            p = categories_subwords_distinct['Subword_freq'] / categories_subwords_distinct['Subword_freq'].sum()
-                                        )
-        my_categories = selection_filtered_df[selection_filtered_df['Category'] == category]
-        other_categories = corpus_word_freq_df[~corpus_word_freq_df.index.isin(selection_base_df[selection_base_df['Category'] == category]['Word'])]
-        other_categories = other_categories[~other_categories.index.isin(selection_base_df[selection_base_df['Category'] == category]['Subword'])]
-        # Iterate over each selected "Subword"
-        for subword in selected_subwords:
-            #other_categories = selection_base_df[selection_base_df['Category'] != category]
-            category_words = my_categories[my_categories['Subword'].isin([subword])]
-            other_categories_weights = other_categories['Word_freq'] / other_categories['Word_freq'].sum()
-            catergory_words_weights = category_words['Word_freq'] / category_words['Word_freq'].sum()
-            # Randomly select 3 "Word" entries weighted by "Word_freq" from other categories
-            selected_wrong_words = np.random.choice(other_categories.index, size=3, replace=False, p=other_categories_weights)
-            # Randomly select 1 "Word" entries weighted by "Word_freq" from my category
-            selected_correct_words = np.random.choice(category_words['Word'], size=1, replace=False, p=catergory_words_weights)
-            # Create a DataFrame with the selected "Subword" and "Word" entries
-            random_questions.append((category, subword, selected_correct_words[0], selected_wrong_words))
+def generate_random_questions(word2vec_model, evaluation_dataset_df, raw_dataset_df, corpus_word_freq_df, K=3):
+    random_answers_per_word = []
+    word2_vec_vocab = word2vec_model.vocab
+    corpus_word_freq_df = corpus_word_freq_df.sort_values(by='Word_freq', ascending=False)
+    p_most_freq = 1000
+    for _, row in evaluation_dataset_df.iterrows():
+        for _, corpus_row in corpus_word_freq_df.iterrows():
+            if (word2_vec_vocab.get(row['Subword']) and word2_vec_vocab.get(corpus_row['Word'])):
+                corpus_word_freq_df['Subword_Similiarity'] = word2vec_model.similarity(row['Subword'], corpus_row['Word'])
+            else:
+                corpus_word_freq_df['Subword_Similiarity']
 
-    return random_questions
+        my_categories_subwords = raw_dataset_df[raw_dataset_df['Category'] == row['Category']]
+        other_categories_words = corpus_word_freq_df[~corpus_word_freq_df.index.isin(my_categories_subwords['Word'])]
+        other_categories_words = other_categories_words[~other_categories_words.index.isin(my_categories_subwords['Subword'])]
+        other_categories_words.loc[:,'Weights'] = other_categories_words['Word_freq'] / other_categories_words['Word_freq'].sum()
+        #Get only the P first most freq words and those with length greater than 3
+        other_categories_words = other_categories_words[:p_most_freq]
+        other_categories_words = other_categories_words[other_categories_words.index.str.len() >= 5]
+        selected_wrong_words = np.random.choice(other_categories_words[:p_most_freq].index, size=K, replace=False)
+        random_answers_per_word.append((row['Category'], row['Subword'], row['Word'], selected_wrong_words))
+
+    return random_answers_per_word
 
 # Download brown corpus
 # nltk.download('brown')
@@ -93,8 +84,11 @@ base_words = set(brown_words)
 corpus_base_word = list(base_words)
 corpus_word_freq = Counter(brown_words)
 corpus_word_freq_df = pd.DataFrame(corpus_word_freq.values(), corpus_word_freq.keys(), columns=["Word_freq"])
-selection_base_df = pd.read_csv("datasets\\dataset.csv")
-random_questions = generate_random_questions(selection_base_df, corpus_word_freq_df)
+evaluation_dataset_df = pd.read_csv("datasets\\dataset_for_evaluation.csv")
+raw_dataset_df = pd.read_csv("datasets\\raw_dataset.csv")
+random_answers_per_word = generate_random_questions(word2vec_model, evaluation_dataset_df, raw_dataset_df, corpus_word_freq_df)
+random_answers_per_word_df = pd.DataFrame(random_answers_per_word, columns=["Category", "Subword", "Word", "Mulitple_Options"])
+random_answers_per_word_df.to_csv("datasets\\dataset_for_evaluation_multiple_options.csv")
 
 TOGETHER_API_KEY = os.environ.get("TOGETHER_API_KEY")
 client = OpenAI(
@@ -103,22 +97,7 @@ client = OpenAI(
 )
 cnt = 1
 response_list = []
-# prompt = ClassificationPrompts.generate_zero_shot(category='animal',
-#                                              word='want')
-# chat_completion = client.chat.completions.create(
-# messages=[
-#     {
-#     "role": "user",
-#     #"content": f"Answer with \"Yes\" or \"No\" only, without explanations. {prompt}",
-#     "content": f"{prompt}",
-#     }
-# ],
-# model="MISTRALAI/MISTRAL-7B-INSTRUCT-V0.2",
-# temperature = 0
-# )
-# print(prompt, chat_completion.choices[0].message.content)
-
-for question in random_questions:
+for question in random_answers_per_word:
     choices = list(question[3])
     choices.insert(cnt%2, question[2])
     # prompt = ClassificationPrompts.generate_zero_shot(category=question[0],
