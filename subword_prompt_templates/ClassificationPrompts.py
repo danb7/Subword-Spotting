25f7d0cf27afdfb9e5cbc7fe67bd40cb5c40edf8
Daniel Bazar <daniel.bazar@gmail.com>
2024-04-25 13:23:56 +0300

    refactoring prompts templates

--- subword_prompt_templates/ClassificationPrompts.py
+++ subword_prompt_templates/ClassificationPrompts.py
@@ -2,39 +2,63 @@ from utils import helpers
 from subword_prompt_templates import few_shot_examples
 
 class ClassificationPrompts():
-    question = '''\
+    def __init__(self, question='''\
 Subword is a standalone word that exists within a longer word that contains it, and its meaning is arbitrary from the longer word one's.
 Question: Does the word "{word}" contains a subword of a {category}?
-Answer: [Your answer here, just Yes/ No, without any explanation or additional text].
-''' # TODO: change []
+Answer: ''',
+answer_aid='''[Your answer here, just Yes/No, without any explanation or additional text]'''):
+        self.question = question
+        self.example = self.question + '{answer}'
+        self.answer_aid = answer_aid
 
-    example = question + '{answer}'
+    
+    def generate(self, technique, category, word, **kwargs):
+        if technique=='zero_shot':
+            prompts = self._generate_zero_shot(category, word, **kwargs)
+        
+        elif technique=='one_shot':
+            prompts = self._generate_one_shot(category, word, **kwargs)
 
+        elif technique=='few_shot':
+            prompts = self._generate_few_shot(category, word, **kwargs)
 
-    def generate_zero_shot(self, category, word):
+        elif technique.lower()=='cot':
+            prompts = self._generate_CoT(category, word, **kwargs)
+
+        elif technique=='decomposite':
+            prompts = self._generate_decomposite(category, word, **kwargs)
+            
+        else:
+            raise Exception("technique must be one of ['zero_shot', 'one_shot', 'few_shot', 'cot', 'decomposite']")
+        
+        return prompts
+
+    def _generate_zero_shot(self, category, word, ans_aid=True):
         formated_question = self.question.format(category=category,
-                                            word=word)
+                                                 word=word)
+        if ans_aid:
+            formated_question += self.answer_aid
         return helpers.fix_a_an(formated_question) # replace "a" to "an" if category start with vowels
 
 
-    def generate_one_shot(self, category, word, positive_answer='Yes'):
+    def _generate_one_shot(self, category, word, positive_answer='Yes.'):
         example_word = few_shot_examples.positive_examples[category][0][0]
         correspond_subword = few_shot_examples.positive_examples[category][0][1]
         one_shot_question =  f'''\
-    ### example question ###
-    {self.example.format(category=category, 
-                    word=example_word, 
-                    answer=positive_answer.format(example_word=example_word, 
-                                                example_subword=correspond_subword,
-                                                category=category))}
+### example question ###
+{self.example.format(category=category,
+                     word=example_word, 
+                     answer=positive_answer.format(example_word=example_word, 
+                                                   example_subword=correspond_subword,
+                                                   category=category))}
 
-    ### actual question ###
-    {self.generate_zero_shot(category, word)}'''
+### actual question ###
+{self._generate_zero_shot(category, word)}'''
         
         return helpers.fix_a_an(one_shot_question)
 
 
-    def generate_few_shot(self, category, word, positive_answer='Yes', negative_answer='No', shots=4):
+    def _generate_few_shot(self, category, word, positive_answer='Yes.', negative_answer='No.', shots=4):
         few_shot_question = ''
         positive_examples = few_shot_examples.positive_examples[category]
         negative_examples = few_shot_examples.negative_examples[category]
@@ -55,33 +79,33 @@ Answer: [Your answer here, just Yes/ No, without any explanation or additional t
                                                                             category=category)) + '\n'*2
         
         few_shot_question += '### actual question ###\n' 
-        few_shot_question += self.generate_zero_shot(category, word)
+        few_shot_question += self._generate_zero_shot(category, word)
         
         return helpers.fix_a_an(few_shot_question)
 
 
-    def generate_CoT(self, category, word, shot='one'):
-        positive_answer = '''Since the word {example_word} contains the subword \
-    {example_subword}, which is a {category}, the correct answer is Yes'''
-        negative_answer = '''Since the word {example_word} doesnt contains any subword \
-    of a {category}, the correct answer is No''' #TODO: change this also.
+    def _generate_CoT(self, category, word, shot='one'):
+        positive_answer = '''Yes. Since the word {example_word} contains the subword \
+{example_subword}, which is a {category}, the correct answer is Yes.'''
+        negative_answer = '''No. Since the word {example_word} doesnt contains any subword \
+of a {category}, the correct answer is No.'''
 
         if shot == 'one':
-            cot_question = self.generate_one_shot(category, word, positive_answer)
+            cot_question = self._generate_one_shot(category, word, positive_answer)
         elif shot == 'few':
-            cot_question = self.generate_few_shot(category, word, positive_answer, negative_answer)
+            cot_question = self._generate_few_shot(category, word, positive_answer, negative_answer)
         else:
             raise Exception("Not valid 'shot' parameter. Need to be 'one' or 'few'")
         
         return helpers.fix_a_an(cot_question)
 
-    def generate_decomposite(self, category, word):
+    def _generate_decomposite(self, category, word):
         decomposite_question = f'''\
 Read the question below and understand what the question is asking for and the criteria for determine the correct answer. 
 Examine the word provided in the question carefully and Break it down into its component parts or subwords.
 Determine if any of the subwords within the word match the name of a {category}. 
 Answer with "Yes" or "No" only, without explanations.
 Verify your answer, double-check your classification to ensure it meets all the requirements specified in the question.
-{self.generate_zero_shot(category, word)}'''
+{self._generate_zero_shot(category, word)}'''
         
         return helpers.fix_a_an(decomposite_question)
--- subword_prompt_templates/MultiChoicePrompts.py
+++ subword_prompt_templates/MultiChoicePrompts.py
@@ -1,64 +1,93 @@
+import random
 from utils import helpers
 from subword_prompt_templates import few_shot_examples
 
 class MultiChoicePrompts():
-    question = '''\
+    def __init__(self, question='''\
 Subword is a standalone word that exists within a longer word that contains it, and its meaning is arbitrary from the longer word one's.
 Question: Which one of the following words contains a subword of a {category}?
     A. {choice1}
     B. {choice2}
     C. {choice3}
     D. {choice4}
-Answer: [Your answer here, just the correspond letter, without any explanation or additional text].
-''' # TODO: change the [] part only on actuals questions
+Answer: ''',
+answer_aid='''[Your answer here, just the correspond letter, without any explanation or additional text]'''):
+        self.question = question
+        self.example = self.question + '{answer}'
+        self.answer_aid = answer_aid
 
-    example = question + '{answer}'
 
-    def generate_zero_shot(self, category, choice1, choice2, choice3, choice4):
+    def generate(self, technique, category, choice1, choice2, choice3, choice4, **kwargs):
+        if technique=='zero_shot':
+            prompts = self._generate_zero_shot(category, choice1, choice2, choice3, choice4, **kwargs)
+        
+        elif technique=='one_shot':
+            prompts = self._generate_one_shot(category, choice1, choice2, choice3, choice4, **kwargs)
+
+        elif technique=='few_shot':
+            prompts = self._generate_few_shot(category, choice1, choice2, choice3, choice4, **kwargs)
+
+        elif technique.lower()=='cot':
+            prompts = self._generate_CoT(category, choice1, choice2, choice3, choice4, **kwargs)
+
+        elif technique=='decomposite':
+            prompts = self._generate_decomposite(category, choice1, choice2, choice3, choice4, **kwargs)
+            
+        else:
+            raise Exception("technique must be one of ['zero_shot', 'one_shot', 'few_shot', 'cot', 'decomposite']")
+        
+        return prompts
+        
+
+    def _generate_zero_shot(self, category, choice1, choice2, choice3, choice4, ans_aid=True):
         formated_question = self.question.format(category=category,
-                                            choice1=choice1, 
-                                            choice2=choice2, 
-                                            choice3=choice3, 
-                                            choice4=choice4)
+                                                 choice1=choice1, 
+                                                 choice2=choice2, 
+                                                 choice3=choice3, 
+                                                 choice4=choice4)
+        if ans_aid:
+            formated_question += self.answer_aid
         return helpers.fix_a_an(formated_question) # replace "a" to "an" if category start with vowels
 
 
-    def generate_one_shot(self, category, choice1, choice2, choice3, choice4, answer=''):
+    def _generate_one_shot(self, category, choice1, choice2, choice3, choice4, answer=''):
         correct_word = few_shot_examples.positive_examples[category][0][0]
         correspond_subword = few_shot_examples.positive_examples[category][0][1]
-        correct_position = 3 #TODO: change this to random
-        choices =  few_shot_examples.negative_examples[category][:3]
+        correct_position = random.randint(1, 4)
+        correct_choice_letter = str(correct_position).translate(str.maketrans("1234", "ABCD"))
+        choices = few_shot_examples.negative_examples[category][:3]
         choices.insert(correct_position-1, correct_word)
         if answer:
-            answer = answer.format(correct_word=correct_word,
-                                correct_subword=correspond_subword,
-                                category=category)
+            answer = answer.format(correct_choice=correct_choice_letter,
+                                   correct_word=correct_word,
+                                   correct_subword=correspond_subword,
+                                   category=category)
         else:
-            answer = correct_word
-        one_shot_question = f'''\
-    ### example question ###
-    {self.example.format(category=category,
-                    choice1=choices[0],
-                    choice2=choices[1],
-                    choice3=choices[2],
-                    choice4=choices[3],
-                    answer=answer)}
+            answer = f'{correct_choice_letter}. {correct_word}'
+        one_shot_question = f'''### example question ###
+{self.example.format(category=category,
+choice1=choices[0],
+choice2=choices[1],
+choice3=choices[2],
+choice4=choices[3],
+answer=answer)}
 
-    ### actual question ###
-    {self.generate_zero_shot(category, choice1, choice2, choice3, choice4)}'''
+### actual question ###
+{self._generate_zero_shot(category, choice1, choice2, choice3, choice4)}'''
         
         return helpers.fix_a_an(one_shot_question)
 
 
-    def generate_few_shot(self, category, choice1, choice2, choice3, choice4, answer='', shots=4):
+    def _generate_few_shot(self, category, choice1, choice2, choice3, choice4, answer='', shots=4):
         few_shot_question = ''
         correct_words = few_shot_examples.positive_examples[category]
         if answer:
-            answers = [answer.format(correct_word=correct_words[i][0],
-                                correct_subword=correct_words[i][1],
-                                category=category) for i in range(shots)]
+            answers = [answer.format(correct_choice=str((i%4)+1).translate(str.maketrans("1234", "ABCD")),
+                                     correct_word=correct_words[i][0],
+                                     correct_subword=correct_words[i][1],
+                                     category=category) for i in range(shots)]
         else:
-            answers = [correct_words[i][0] for i in range(shots)]
+            answers = [f'{str((i%4)+1).translate(str.maketrans("1234", "ABCD"))}. {correct_words[i][0]}' for i in range(shots)]
         for i in range(shots):
             choices = few_shot_examples.negative_examples[category][i*3:i*3+3]
             choices.insert(i%4, correct_words[i][0])
@@ -70,31 +99,31 @@ Answer: [Your answer here, just the correspond letter, without any explanation o
                                                 choice4=choices[3],
                                                 answer=answers[i]) + '\n'*2
         few_shot_question += '### actual question ###\n' 
-        few_shot_question += self.generate_zero_shot(category, choice1, choice2, choice3, choice4)
+        few_shot_question += self._generate_zero_shot(category, choice1, choice2, choice3, choice4)
 
         return helpers.fix_a_an(few_shot_question)
 
 
-    def generate_CoT(self, category, choice1, choice2, choice3, choice4, shot='one'):
-        cot_answer = '''Since the word {correct_word} contains the subword \
-    {correct_subword}, which is a {category}, the correct answer is {correct_word}''' # TODO: change to (for example): A. [explanation]
+    def _generate_CoT(self, category, choice1, choice2, choice3, choice4, shot='one'):
+        cot_answer = '''{correct_choice}. Since the word {correct_word} contains the subword \
+{correct_subword}, which is a {category}, the correct answer is {correct_choice}. {correct_word}.'''
         if shot == 'one':
-            cot_question = self.generate_one_shot(category, choice1, choice2, choice3, choice4, cot_answer)
+            cot_question = self._generate_one_shot(category, choice1, choice2, choice3, choice4, cot_answer)
         elif shot == 'few':
-            cot_question = self.generate_few_shot(category, choice1, choice2, choice3, choice4, cot_answer)
+            cot_question = self._generate_few_shot(category, choice1, choice2, choice3, choice4, cot_answer)
         else:
             raise Exception("Not valid 'shot' parameter. Need to be 'one' or 'few'")
         
         return helpers.fix_a_an(cot_question)
 
 
-    def generate_decomposite(self, category, choice1, choice2, choice3, choice4):
+    def _generate_decomposite(self, category, choice1, choice2, choice3, choice4):
         decomposite_question = f'''\
 Read the question below and understand what the question is asking for and the criteria for selecting the correct answer. 
 Examine each word provided in the options carefully and Break down each word into its component parts or subwords.
 Determine if any of the subwords within each word match the name of a {category}. 
 Choose the word that contains a subword of a {category} according to the criteria given in the question.
 Verify your answer, double-check your selection to ensure it meets all the requirements specified in the question.
-{self.generate_zero_shot(category, choice1, choice2, choice3, choice4)}'''
+{self._generate_zero_shot(category, choice1, choice2, choice3, choice4)}'''
         
         return helpers.fix_a_an(decomposite_question)
